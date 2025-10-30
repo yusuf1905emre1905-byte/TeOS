6b0a57a2c0817e3ac747921b267ae89e477258d2
cOS <yusuf1905emre1905@gmail.com>
2025-10-30 22:27:34 +0300

    Create characters.c

---
+++ takky_run/characters.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "characters.c"
+
+void play_cubby_game() {
+    printf("Mini Game: Cubby Top Yakalama başlatıldı!\n");
+    // Oyun mekaniği burada olacak
+}
+
+void play_doggy_game() {
+    printf("Mini Game: Doggy Engellerden Kaçma başlatıldı!\n");
+}
+
+void play_borty_game() {
+    printf("Mini Game: Borty Kart Eşleştirme başlatıldı!\n");
+}
