8867262f0b75a51ef671b085d5ea0ad7adc6ad15
cOS <yusuf1905emre1905@gmail.com>
2025-10-30 22:19:59 +0300

    Create drivers.c

---
+++ drivers/drivers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void init_keyboard() {
+    // Klavye sürücüsü başlat
+    printf("Keyboard initialized.\n");
+}
+
+void init_mouse() {
+    // Fare sürücüsü başlat
+    printf("Mouse initialized.\n");
+}
+
+void init_display() {
+    // Ekran sürücüsü başlat
+    printf("Display initialized.\n");
+}
