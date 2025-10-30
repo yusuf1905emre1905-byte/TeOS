fdea59f4cd2120db2fdcdbe85f1f90aac5b1a34d
cOS <yusuf1905emre1905@gmail.com>
2025-10-30 22:20:45 +0300

    Add GUI initialization and taskbar loading functions

---
+++ gui/window.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void gui_init() {
+    printf("GUI initialized with window layout.\n");
+}
+
+void gui_update() {
+    // GUI'yi sürekli yenile
+}
+
+void load_taskbar() {
+    printf("Taskbar loaded with Start Menu.\n");
+}
+
+void load_app_icons() {
+    printf("Application icons loaded.\n");
+}
